682adc64f027cfe1dbbab214cb3eedee6155c74e
atSartor <atsartor@crimson.ua.edu>
2020-10-08 19:51:09 -0500

    Debug

--- app/controllers/API/V1/validate.rb
+++ app/controllers/API/V1/validate.rb
@@ -1,20 +1,7 @@
-#validator = GoogleIDToken::Validator.new
-#begin
-# payload = validator.check(token, required_audience, optional_client_id)
-# email = payload['email']
-#rescue GoogleIDToken::ValidationError => e
-#report "Cannot validate: ${e}"
-#end
-module API
-  module V1
-    class Validator < Grape::API
-      include API::V1::Defaults
-      resource :validator do
-        desc "Return all recipes"
-        get "" do
-          Recipe.all
-        end
-      end
-    end
-  end
+validator = GoogleIDToken::Validator.new
+begin
+ payload = validator.check(token, required_audience, optional_client_id)
+ email = payload['email']
+rescue GoogleIDToken::ValidationError => e
+report "Cannot validate: ${e}"
 end
