8905d6e258b18515cf7be90166d78276b702edcb
atSartor <atsartor@crimson.ua.edu>
2020-10-08 19:21:00 -0500

    Changed Puma settings

--- config/puma.rb
+++ config/puma.rb
@@ -7,7 +7,6 @@
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
-
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 port        ENV.fetch("PORT") { 3000 }
@@ -25,14 +24,16 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+preload_app!
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
 # preload_app!
-
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
