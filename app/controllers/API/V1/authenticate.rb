3b187f46828a5298fbe6d9ff0e38b6b22f08d2c5
Andrew Sartor <62850255+atSartor@users.noreply.github.com>
2020-11-05 19:55:10 -0600

    Added ability to add users by ID token. Added API mounting points for the Favorite and Authenticate methods. TODO: test added users API endpoint

--- app/controllers/API/V1/authenticate.rb
+++ app/controllers/API/V1/authenticate.rb
@@ -7,7 +7,19 @@ module API
 
       resource :authenticate do
         post do
-
+          #this is very much a dummy authentication system now
+          # TODO: need to eventually verify the received token was generated
+          # by Google -- will add in a future version.
+          # also this scheme of verifying a token is not a good way
+          # to identify a user long term, as tokens change.. often
+          if UserToken.exists?(idToken: :id_token)
+            #TODO: verify token is authentic
+          else
+            #TODO: verify token authentic
+            # Register token
+            @new_user = UserToken.new(idToken: :id_token)
+            @new_user.save
+          end
         end
       end
     end
--- app/controllers/API/V1/base.rb
+++ app/controllers/API/V1/base.rb
@@ -4,6 +4,8 @@ module API
       mount API::V1::Users
       mount API::V1::Recipes
       mount API::V1::ShowRecipes
+      mount API::V1::Authenticate
+      mount API::V1::Favorite
     end
   end
 end
